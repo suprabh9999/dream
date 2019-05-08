eeb738b3b67d9823c4d9b8a8be221e829b6fd40c
Mona Lisa <official.suprabh@gmail.com>
2019-05-08 21:17:18 +0530

    Updates

---
+++ precache-manifest.29e534834e71974e8ab10c22d43b9e4b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfab3ccf55df086a622fc93df910eaba",
+    "url": "/dream/index.html"
+  },
+  {
+    "revision": "7517de203ab25264b24f",
+    "url": "/dream/static/css/main.8220d4a0.chunk.css"
+  },
+  {
+    "revision": "46d3ea56e1bbeef365ae",
+    "url": "/dream/static/js/2.8871ae74.chunk.js"
+  },
+  {
+    "revision": "7517de203ab25264b24f",
+    "url": "/dream/static/js/main.d77995aa.chunk.js"
+  },
+  {
+    "revision": "9063a727ff4c95b46a22",
+    "url": "/dream/static/js/runtime~main.263f3cbf.js"
+  },
+  {
+    "revision": "aa2603332b8ca35d44061679d1d79833",
+    "url": "/dream/static/media/dood.aa260333.jpg"
+  },
+  {
+    "revision": "b0336eb03d0ee5d91f1160f4f0e88e55",
+    "url": "/dream/static/media/dood1.b0336eb0.jpg"
+  },
+  {
+    "revision": "482ca106ede9f6230511e9e574fa44c1",
+    "url": "/dream/static/media/dood2.482ca106.jpg"
+  },
+  {
+    "revision": "35b698952df61cc577a6e08be3a6c0fc",
+    "url": "/dream/static/media/people1.35b69895.jpg"
+  }
+]);
